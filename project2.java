381f8195ced85b53ebab495c3201097e11a35e40
iansidway <iansidway@gmail.com>
2017-02-17 15:15:44 +0000

    Add files via upload

---
+++ project2.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+
+public class project2{
+
+	public static void main(String [] args){
+		String[][] userPassArray=readUserPassFile();
+		String username;
+		String password;
+		int tries  = 0;
+		while(tries<3){
+			username = JOptionPane.showInputDialog(null, "Enter your username");
+			password = JOptionPane.showInputDialog(null, "Enter your password");
+			if(checkValidation(username, password)==true) break;
+			JOptionPane.showMessageDialog(null, " Invalid username or password" );
+			tries++;
+		}
+
+
+
+
+	}
+	public static String[][] readUserPassFile(){
+
+		//intake txt file
+		//lets see if we can do this multidimesional guys
+
+
+	}
+	public static boolean checkValidation(String username, String password, String[][] userPassArray){
+		boolean isMatch=false;
+		//sift through array to find a match, find username first, if not found end search
+
+		return isMatch;
+
+
+	}
+	public static void questions(){
+		String[][] questions=readQuestionFile();
+
+
+
+
+	}
+	public static String[][] readQuestionFile(){
+
+
+
+	}
+}
