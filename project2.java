01308d59cecbd89f70db6d1afb03ea4c6077296d
MrSwayne <A.Swayne92@gmail.com>
2017-02-17 23:30:52 +0000

    Update project2.java
    I checked mine and Ian's code and took pieces from each and joined it together, also cleaning it u in the process to make it more neat, I'm pretty sure it all works up until you choose a topic, if you find anything wrong then let me know. 
    
    All we need now is the text file with all the questions and stuff in it, and then we just output it to the user.

--- project2.java
+++ project2.java
@@ -1,53 +1,113 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
-public class project2{
+public class Project2
+{
+	public static void main(String[] args) throws IOException
+	{
+		boolean validDetails = false;
+		int tries = 0;
+		String username, password;
+		
+		//loop to get username and password, if correct username and password is supplied, program is launched.
+		while (!validDetails && tries < 3)
+		{	
+			//Takes in username and password from user, if the user presses cancel then the program will exit
+			username = JOptionPane.showInputDialog(null, "Please insert a username " + "(attempt " + (tries + 1) + ")");	
+			
+			if(username != null)
+			{
+				//put the username to lowercase because usernames should not be case sensitive
+				username = username.toLowerCase();
+				
+				password = JOptionPane.showInputDialog(null, "Please insert a password " + "(attempt " + (tries + 1) + ")");
+				
+				if(password!= null)
+				{
+					//checks if the user and password supplied is correct
+					if(checkValidation(username, password))
+					{
+						//if it is correct then it launches the program
+						validDetails = true;
+						programLauncher(username);
+				
+					}
+					//if it isn't correct then it tells the user and then starts again with the tries counter incremented.
+					else
+					{
+						if(tries == 2)		JOptionPane.showMessageDialog(null, "You have entered an incorrect username and password 3 times, the program will now exit.");
+						else							JOptionPane.showMessageDialog(null, "Invalid username or password, please try again.");
+					}
+				}		//if the user presses cancel, tries should go to 3 so it won't reloop
+						else						tries = 3;
+			}
+						else						tries = 3;
+				//increment tries everytime it goes into the loop
+				tries++;
+		}
+	}
 
-	public static void main(String [] args){
-		String[][] userPassArray=readUserPassFile();
-		String username;
-		String password;
-		int tries  = 0;
-		boolean success=false;
-		while(tries<3 && success=false){
-			username = JOptionPane.showInputDialog(null, "Enter your username");
-			password = JOptionPane.showInputDialog(null, "Enter your password");
-			if(checkValidation(username, password)==true) success=true;
-			JOptionPane.showMessageDialog(null, " Invalid username or password" );
-			tries++;
+	//checks validation of input
+	public static boolean checkValidation(String username, String password) throws IOException
+	{
+		boolean isValid = false;
+		
+		//Takes in input from a file
+		FileReader file = new FileReader("users.txt");
+		Scanner in = new Scanner(file);
+		
+		String[] data;
+		
+		while(in.hasNext())
+		{
+			//splits the data, subset 0 is username, subset 1 is 
+			data = in.nextLine().split(",");
+			
+			//checks if the username and password supplied matches what's on file
+			if(username.matches(data[0]) && password.matches(data[1]))	isValid = true;
+		}
+		in.close();
+		file.close();
+		return isValid;
+	}
+	//gets a choice from the user and calls the getQuestions method if input is correct
+	public static void programLauncher(String username) throws IOException
+	{
+		String choice = JOptionPane.showInputDialog(null, "Hi " + username + ", welcome to the Multiple Choice Practice Test!\nPlease choose a topic below!\n1... Arrays\n2... Syntax\n3... String Manipulation\n4... <no idea yet>");
+		String pattern = "[1-4]{1}";
+		while(choice != null)
+		{
+			if(choice.matches(pattern))
+			{
+				getQuestions(Integer.parseInt(choice));
+			}
+			else
+				JOptionPane.showMessageDialog(null, "Error, please input a number between 1 and 4");
 		}
 		
-
-
-
-
 	}
-	public static String[][] readUserPassFile(){
-
-		//intake txt file
-		//lets see if we can do this multidimesional guys
-
-
-	}
-	public static boolean checkValidation(String username, String password, String[][] userPassArray){
-		boolean isMatch=false;
-		//sift through array to find a match, find username first, if not found end search
-
-		return isMatch;
-
-
-	}
-	public static int questions(){
-		String[][] questions=readQuestionFile();
-		int points=0;
+	
+	//Gets questions for the topic selected by the user
+	public static void getQuestions(int topic) throws IOException
+	{
+		FileReader file = new FileReader("questions.txt");
+		Scanner in = new Scanner(file);
 		
-
-
-		return points;
-
+		String[] data;
+		
+		//splits the data so we can check if the topic corresponds with subscript 0 etc.
+		while(in.hasNext())
+		{
+			//TODO
+			//Splits the data, subset 0 = topic, subset 1 = question number, subset 2 = question, subset 4 = correct answer, subset 5 = possible answer 1, subset 6 = possible answer 2, subset 7 = possible answer 3, subset 8 = explanation
+		}
+		
+	
+		//TODO
+		//a method to choose and randomise the questions and answers.
+		//track score in the users.txt file
+		//output current and overall score to the user
 	}
-	public static String[][] readQuestionFile(){
 
-
-
-	}
 }
